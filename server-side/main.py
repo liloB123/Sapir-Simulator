2e70842e19d2eb4d6b5ee0b71e42171a486d146a
lihi <lihibloy327@gmail.com>
2025-06-16 11:44:57 +0300

    FastAPI basic config

---
+++ server-side/main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+import uvicorn
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "Ofir and Lihi"}
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+
