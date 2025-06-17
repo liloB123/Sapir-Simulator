75f12427ffec748cb2f653db4c48e1d25b73f6cf
lihi <lihibloy327@gmail.com>
2025-06-17 21:48:07 +0300

    remove ouline from toggle

--- frontend/src/components/Layer.tsx
+++ frontend/src/components/Layer.tsx
@@ -39,7 +39,7 @@ const Layer: React.FC<LayerProps> = ({ name, icon, color, description }) => {
                     checked={isActive}
                 />
                 <div
-                    className="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300
+                    className="relative w-11 h-6 bg-gray-200 peer-focus:outline-none
                     dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full
                     rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute
                     after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full
