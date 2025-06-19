fbcb404141c77be80da3b3b5ddd9625fd7ed97df
BenM61 <benmassarano4@gmail.com>
2025-06-19 16:13:03 +0300

    raster UK

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,120 +1,24 @@
-import type { FeatureCollection } from "geojson";
 import React from "react";
-import Map, { Layer, Source, type LayerProps } from "react-map-gl/maplibre";
+import Map, {
+  Layer,
+  Source,
+  type LayerProps,
+  type SourceProps,
+} from "react-map-gl/maplibre";
 
-const parks: FeatureCollection = {
-  type: "FeatureCollection",
-  features: [
-    {
-      type: "Feature",
-      properties: { name: "Tel Aviv" },
-      geometry: {
-        type: "Point",
-        coordinates: [34.7818, 32.0853],
-      },
-    },
-    {
-      type: "Feature",
-      properties: { name: "Jerusalem" },
-      geometry: {
-        type: "Point",
-        coordinates: [35.2137, 31.7683],
-      },
-    },
-    {
-      type: "Feature",
-      properties: { name: "Haifa" },
-      geometry: {
-        type: "Point",
-        coordinates: [34.9896, 32.794],
-      },
-    },
+const rasterSource: SourceProps = {
+  type: "raster",
+  tiles: [
+    "https://api.maptiler.com/tiles/uk-osgb1919/{z}/{x}/{y}.jpg?key=sv1aPMPj3scmsnN1HtxP",
   ],
+  tileSize: 256,
 };
 
-const parksLayer: LayerProps = {
-  id: "parksLayer",
-  type: "circle",
+const rasterLayer: LayerProps = {
+  id: "distance-heat",
+  type: "raster",
   paint: {
-    "circle-radius": 10,
-    "circle-color": "#FF0000",
-  },
-};
-
-const trails: FeatureCollection = {
-  type: "FeatureCollection",
-  features: [
-    {
-      type: "Feature",
-      properties: { name: "Trail A" },
-      geometry: {
-        type: "LineString",
-        coordinates: [
-          [34.9, 32.086],
-          [34.7, 32.187],
-        ],
-      },
-    },
-    {
-      type: "Feature",
-      properties: { name: "Trail B" },
-      geometry: {
-        type: "LineString",
-        coordinates: [
-          [35.3137, 31.7683],
-          [35.3145, 31.969],
-        ],
-      },
-    },
-    {
-      type: "Feature",
-      properties: { name: "Trail C" },
-      geometry: {
-        type: "LineString",
-        coordinates: [
-          [34.8, 32.794],
-          [34.7, 32.996],
-        ],
-      },
-    },
-  ],
-};
-
-const trailsLayer: LayerProps = {
-  id: "trailsLayer",
-  type: "line",
-  paint: {
-    "line-color": "#00FF00",
-    "line-width": 4,
-  },
-};
-
-const neighborhoods: FeatureCollection = {
-  type: "FeatureCollection",
-  features: [
-    {
-      type: "Feature",
-      properties: { name: "Neighborhood A" },
-      geometry: {
-        type: "Polygon",
-        coordinates: [
-          [
-            [34.9, 32.3],
-            [34.9, 32.11],
-            [34.81, 32.11],
-          ],
-        ],
-      },
-    },
-  ],
-};
-
-const neighborhoodsLayer: LayerProps = {
-  id: "neighborhoodsLayer",
-  type: "fill",
-  paint: {
-    "fill-opacity": 1,
-    "fill-color": "#0000FF",
+    "raster-opacity": 0.7,
   },
 };
 
@@ -129,14 +33,8 @@ const App: React.FC = () => {
       style={{ width: "100vw", height: "100vh" }}
       mapStyle="https://demotiles.maplibre.org/style.json"
     >
-      <Source id="parks" type="geojson" data={parks}>
-        <Layer {...parksLayer} />
-      </Source>
-      <Source id="trails" type="geojson" data={trails}>
-        <Layer {...trailsLayer} />
-      </Source>
-      <Source id="neighborhoods" type="geojson" data={neighborhoods}>
-        <Layer {...neighborhoodsLayer} />
+      <Source id="uk-raster" {...rasterSource}>
+        <Layer {...rasterLayer} />
       </Source>
     </Map>
   );
