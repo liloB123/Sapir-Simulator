f262a7729fb87c3269e688a8e82c8fa52a0f0166
Asaf <asafucho@gmail.com>
2025-06-23 18:39:50 +0300

    combining 2 input layers

--- backend/raster_provider.py
+++ backend/raster_provider.py
@@ -7,52 +7,73 @@ import uvicorn
 import numpy as np
 import matplotlib.pyplot as plt
 from PIL import Image
+from shapely import LineString, Point, distance
+import random
+
 
 app = FastAPI()
 
 
-def normalize_raster(rstr: np.ndarray, number_of_sources: int):
+def colorize_raster(raster: np.ndarray):
     cmap = plt.get_cmap("inferno")
-
-    print(np.min(rstr), np.max(rstr))
-    # normalized = (rstr - np.min(rstr)) / (np.max(rstr) - np.min(rstr))
-
-    # min_dist, max_dist = 1, 1200000
-    # normalized = (rstr - min_dist) / (max_dist - min_dist)
-
-    # values: np.ndarray = np.copy(rstr)
-    # values[values < 1] = 1
-    # normalized = 1 / values
-
-    # normalized = np.exp(-rstr / 50000)
-
-    normalized = rstr / number_of_sources
-
-    rgba = cmap(normalized)
+    rgba = cmap(raster)
     rgb = (rgba[:, :, :3] * 255).astype("uint8")
     return rgb
+
+
+def dists_from_points_layer(grid_x, grid_y):
+    values = np.zeros_like(grid_x)
+
+    lons = [random.uniform(33.75, 39.375) for _ in range(15)]
+    lats = [random.uniform(27.1, 31.9) for _ in range(15)]
+    # source_points = [(35, 30), (36, 28.7), (38.6, 30.2)]
+    for ref_lon, ref_lat in zip(lons, lats):
+        ref_x, ref_y = lnglat_to_meters(ref_lon, ref_lat)
+        dists = np.sqrt((grid_x - ref_x)**2 + (grid_y - ref_y)**2)
+        values += np.exp(-dists / 50000)
+
+    # values = values / len(lons)
+    return values
+
+
+def dists_from_line_layer(grid_x, grid_y):
+    source_line = [(35, 28.5), (36, 28), (39, 31)]
+    mercator_line = [lnglat_to_meters(lon, lat) for lon, lat in source_line]
+    line = LineString(mercator_line)
+
+    flat_x = grid_x.ravel()
+    flat_y = grid_y.ravel()
+    points = np.array([Point(x, y) for x, y in zip(flat_x, flat_y)])
+
+    distances = distance(points, line)
+    values_flat = np.exp(-distances / 50000)
+    values = values_flat.reshape(grid_x.shape)
+
+    return values
+
+
+def join_layers(layers: list):
+    joined_layer = np.max(layers, axis=0)
+    return joined_layer
 
 
 def render_tile(x, y, z):
     tile = mercantile.tile(x, y, z)
     bbox = mercantile.bounds(tile)
     print(bbox)
-    x0, y0 = lnglat_to_meters(bbox.west, bbox.south)
-    x1, y1 = lnglat_to_meters(bbox.east, bbox.north)
+    west_utm, south_utm = lnglat_to_meters(bbox.west, bbox.south)
+    east_utm, north_utm = lnglat_to_meters(bbox.east, bbox.north)
 
-    xs = np.linspace(x0, x1, 256)
-    ys = np.linspace(y0, y1, 256)
+    xs = np.linspace(west_utm, east_utm, 256)
+    ys = np.linspace(south_utm, north_utm, 256)
     grid_x, grid_y = np.meshgrid(xs, ys)
 
-    values = np.zeros_like(grid_x)
+    layer1 = dists_from_points_layer(grid_x, grid_y)
+    layer2 = dists_from_line_layer(grid_x, grid_y)
 
-    source_points = [(35, 28.5), (36, 28), (39, 31)]
-    for ref_lon, ref_lat in source_points:
-        ref_x, ref_y = lnglat_to_meters(ref_lon, ref_lat)
-        dists = np.sqrt((grid_x - ref_x)**2 + (grid_y - ref_y)**2)
-        values += np.exp(-dists / 50000)
+    joined_layer = join_layers([layer1, layer2])
 
-    rgb_img = normalize_raster(values, len(source_points))
+    rgb_img = colorize_raster(joined_layer)
 
     return Image.fromarray(rgb_img, mode='RGB')
 
