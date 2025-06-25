3ebccc6eabe0ceab4f1a6b660c9c3f801b7d88fa
lihi <lihibloy327@gmail.com>
2025-06-25 17:29:13 +0300

    fix design

--- frontend/src/components/LayersMenu/LayersCombinationSection.tsx
+++ frontend/src/components/LayersMenu/LayersCombinationSection.tsx
@@ -4,18 +4,16 @@ import CombinationOption from "./CombinationsOption";
 
 const LayersCombinationSection: FC = () => {
     return (
-        <div className="p-2 border border-white/30 rounded-lg bg-slate-800/30 h-[35%] flex flex-col justify-evenly">
+        <div className="p-2 border border-white/30 rounded-lg bg-slate-800/30 h-[30%] flex flex-col justify-evenly">
             <h2 className="text-l font-bold text-white self text-right">שיטת איחוד מפות החום</h2>
-            <div className="border rounded-md p-4">
-                <div className="flex flex-col items-center gap-3">
-                    {layersCombintaionOptions.map((option) => (
-                        <CombinationOption
-                            key={option.label}
-                            label={option.label}
-                            description={option.description}
-                        />
-                    ))}
-                </div>
+            <div className="flex flex-col items-center gap-3">
+                {layersCombintaionOptions.map((option) => (
+                    <CombinationOption
+                        key={option.label}
+                        label={option.label}
+                        description={option.description}
+                    />
+                ))}
             </div>
         </div>
     )
